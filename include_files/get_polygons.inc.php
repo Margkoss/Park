a6da27daa03733cf29ffdef8a3a3960bb0b1e1e2
Tommen <tomkaraisk@gmail.com>
2019-01-24 00:09:20 +0200

    added php script for retrieving polygons data from DB

--- include_files/get_polygons.inc.php
+++ include_files/get_polygons.inc.php
@@ -1,3 +1,34 @@
 <?php
 
-include_once ("dataBaseHandler.inc.php");
+    include_once ("dataBaseHandler.inc.php");
+
+?>
+
+<pre>
+
+<?php
+
+
+    //Database query to get the coordinates column
+    $sql = "SELECT coordinates FROM kml_data;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+
+    if ($resultCheck > 0) {
+
+        
+        while ($row = mysqli_fetch_assoc($result)) {
+            //Seperate the coordinates from each other in an array
+            $break = explode(" " ,$row['coordinates']);
+            
+            //Seperate the array in to x,y coordinates
+            for ($i=0; $i<sizeof($break); $i++) {
+                $break[$i] = explode("," , $break[$i]);
+            }
+            //debug
+            print_r($break);
+        }
+
+    }
+?>
