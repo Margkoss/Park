ead67d646407b98e0f6a9dd8f53511dccb6f58bc
Tommen <tomkaraisk@gmail.com>
2019-02-10 22:16:05 +0200

    Added PHPMailer, included sendEmail

---
+++ include_files/send_Email.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+#Submit button press check
+if (isset($_POST['submit']))
+{
+    #PHPMailer initialization
+    include_once('../PHPMailer/class.phpmailer.php');
+    require_once('../PHPMailer/class.smtp.php');
+
+
+    #Aquiring User's Credentials   
+    $name = ($_POST['name']);
+    $email =  ($_POST['email']);
+    $msg =  ($_POST['message']);
+
+    #Php mail setup
+    $mail = new PHPMailer(true);
+    $mail->IsSMTP();
+    $mail->CharSet = 'UTF-8';
+    $mail->Host       = "ssl://smtp.gmail.com"; // SMTP server          
+    $mail->SMTPAuth   = true;                  // enable SMTP authentication
+    $mail->Port       = 465;                    // set the SMTP port for the GMAIL server
+    $mail->SMTPSecure = 'ssl';
+    $mail->Username   = "park.reportbug@gmail.com"; // SMTP account username 
+    $mail->Password   = "tmbpark2019";        // SMTP account password 
+    $mail->From = $email;
+    $mail->FromName = $name;
+    $mail->AddAddress($email); 
+    $mail->IsHTML(true);
+    $mail->Subject = 'Park Bug Report';
+    $mail->Body    =  'Your email has been sent to one of our administrators and is currently waiting to be reviewed! Thank you for making Park better.';
+
+
+    #Error Message Display Info
+    if(!$mail->Send())
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    exit;
+
+}
